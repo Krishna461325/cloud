3ebef9772292a03a38612a01a7c35d3dbbcfa1b1
Eswar <119907459+Krishna461325@users.noreply.github.com>
2023-02-02 13:24:22 -0500

    Update sec.py

--- sec.py
+++ sec.py
@@ -1,22 +1,28 @@
 import boto3
 
 class SecretManager:
-    def __init__(self, account, secret_name):
-        self.client = boto3.client('secretsmanager')
+    def __init__(self, account):
         self.account = account
-        self.secret_name = secret_name
+        self.client = boto3.client("secretsmanager")
 
     def get_credentials(self):
-        # retrieve secrets from AWS Secrets Manager
-        response = self.client.get_secret_value(SecretId=self.secret_name)
-        secret = response['SecretString']
-        # parse the secret string and return username and password
-        return {
-            "username": secret['username'],
-            "password": secret['password']
-        }
-
-# sample program to call the class
-secret_manager = SecretManager("my_account", "my_secret_name")
-credentials = secret_manager.get_credentials()
-print(credentials)
+        secret_name = self.account + "_credentials"
+        try:
+            response = self.client.get_secret_value(SecretId=secret_name)
+        except self.client.exceptions.ResourceNotFoundException as e:
+            print("Error: The requested secret " + secret_name + " was not found")
+            return None
+        except self.client.exceptions.InvalidRequestException as e:
+            print("Error: The request was invalid due to:", e)
+            return None
+        except self.client.exceptions.InvalidParameterException as e:
+            print("Error: The request had invalid params:", e)
+            return None
+        else:
+            # Decrypts secret using the associated KMS CMK.
+            # Depending on whether the secret is a string or binary, one of these fields will be populated.
+            if "SecretString" in response:
+                secret = response["SecretString"]
+            else:
+                secret = response["SecretBinary"]
+            return eval(secret)
